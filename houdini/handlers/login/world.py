0f86bd1a47787b223abcaaaca9ae8b62b7b6407e
Allinol <leet.cc211@gmail.com>
2021-04-25 16:40:07 +0700

    update so the game needs to relog

--- houdini/handlers/login/world.py
+++ houdini/handlers/login/world.py
@@ -108,7 +108,7 @@ async def snow_login(p, environment, p_id: int, token: str):
         p.logger.info(f'{p_id} failed to login L')
         return await p.send_tag('S_LOGINDEBUG', 'user code 1000')
 
-    if p.server_world:
+    if p.snow_world:
         p.world_name = server_token['match_session']
         await p.server.redis.delete(token)
 
