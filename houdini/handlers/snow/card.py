82b3b755ea8013cae4e8e5b12735eb81e5eef3fe
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2021-06-11 09:36:12 +0700

    Update card.py

--- houdini/handlers/snow/card.py
+++ houdini/handlers/snow/card.py
@@ -5,7 +5,7 @@ from houdini.handlers import FrameworkPacket
 @handlers.handler(FrameworkPacket('cardClick'))
 async def handle_select_card(p, cardId: int = 0, element: str = None, **data):
     ninja_element = p.snow_ninja.ninja.CardElement.value
-    if element != ninja_element or element is None or not cardId:
+    if element != ninja_element and element is None and not cardId:
         return await p.send_tag('O_WOW', 'you have no job don\'t you')
 
     p.snow_ninja.selected_card = p.server.cards[cardId]
