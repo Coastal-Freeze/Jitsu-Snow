6614025e6174cfc5adc7aa7c892d0215f2d34ad5
AllinolCP <leet.cc211@gmail.com>
2021-07-09 20:08:37 +0700

    fix file not included

---
+++ snow/handlers/login/handshake.py
@@ -0,0 +1,21 @@
+from snow.events import event, TagPacket
+
+
+@event.on(TagPacket('/version'))
+async def handle_snow_version_check(p):
+    await p.send_tag('S_VERSION', 'FY15-20150206 (4954)r', '73971eecbd8923f695303b2cd04e5f70',
+                     'Tue Feb  3 14:11:56 PST 2015',
+                     '/var/lib/jenkins/jobs/BuildPlatform/workspace/metaserver_source/dimg')
+
+
+@event.on(TagPacket('/place_context'))
+async def handle_server_context(p, world, parameters):
+    parameters_parsed = parameters.split('&')
+    for parameter in parameters_parsed:
+        curr_param = parameter.split('=')
+        if curr_param[0] == 'tipMode':
+            p.tip_mode = bool(curr_param[1])
+        elif curr_param[0] == 'muted':
+            p.muted = bool(curr_param[1])
+        else:
+            p.media_url = curr_param[1]
